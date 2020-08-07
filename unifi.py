9cdd8849bba231413e6b33381493bf3f0946c6c3
William George Cook <wgeorgecook@gmail.com>
2020-08-06 19:28:26 -0700

    Better everything

--- unifi.py
+++ unifi.py
@@ -9,6 +9,7 @@ from time import sleep
 
 init(os.environ.get("PRIVATE_KEY"))
 pushClient = Client("iPhone")
+pattern = re.compile('"inventory_quantity":[0-999999]')
 
 url = "https://store.ui.com/collections/unifi-protect-cameras/products/uvc-g4-doorbell"
 logger = logging.getLogger()
@@ -19,29 +20,27 @@ handler.setFormatter(formatter)
 logger.addHandler(handler)
 logger.setLevel(logging.INFO)
 
-
+class BadResponseException(Exception):
+    pass
 
 def check_inventory():
     resp = requests.get(url)
     if resp.status_code != 200:
-        logger.info("Did not get a valid response from unifi")
+        logger.info("Did not get a valid response from unifi, exiting")
         pushClient.send_message("Cannot reach Unifi Website!", "Unifi Doorbell Inventory Check", os.environ.get("CLIENT_ID"),"1", "", "2", url, "Open UI Store", "0", "1", "120", "1200", "0", "", "", "")
-        return
-    soup = BeautifulSoup(resp.text, "html.parser")
+        raise BadResponseException
 
-    pattern = re.compile('"inventory_quantity":[0-999999]')
-
-    script = soup.find("script", text=pattern)
+    script = BeautifulSoup(resp.text, "html.parser").find("script", text=pattern)
     if script:
         match = pattern.search(str(script))
         if match:
-            match_ob = match.group()
-            if match_ob[-1].strip() == "0":
-                logger.info("No inventory, checking again in 30 seconds")
-                sleep(30)
-                return check_inventory()
+            if match.group()[-1].strip() == "0":
+                logger.info("No inventory, checking again in one minute")
+                sleep(60)
+                logging.info("exiting...")
+                sys.exit() # In Docker we can exit and it'll restart the container to prevent memory leaks
             else:
-                logger.info(f"Quantity available: {match_ob[-1]}")
+                logger.info(f"Quantity available: {match.group()[-1]}")
                 pushClient.send_message("Doorbell available!", "Unifi Doorbell Inventory Check", os.environ.get("CLIENT_ID"),"1", "", "2", url, "Open UI Store", "0", "1", "120", "1200", "0", "", "", "")
                 return
 
@@ -51,4 +50,6 @@ try:
 except KeyboardInterrupt:
     # quit
     logger.info("Interrupt received, shutting down")
+    sys.exit()
+except BadResponseException:
     sys.exit()
